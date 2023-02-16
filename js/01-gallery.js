d8209770155071d8e89f86eebcaf5571a6216496
Artem <artembariga5@gmail.com>
2023-02-16 19:53:24 +0200

    add link basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+function renderImage (galleryItems) {
+    const imagesGallery = []
+
+    for(const elem of galleryItems) {
+        imagesGallery.push(`<img src='${elem.preview}' alt='${elem.description}' style='width: 340px'/>`);
+        console.log(elem.original);
+    }
+
+    return imagesGallery.join('');
+}
+
+listEl.insertAdjacentHTML('beforeend', renderImage(galleryItems));
+
+import * as basicLightbox from 'basiclightbox';
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+
+instance.show();
