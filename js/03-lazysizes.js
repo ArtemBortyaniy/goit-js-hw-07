5a9b54714c37a302a025a24576ebbff6684f41b0
Artem <artembariga5@gmail.com>
2023-02-17 15:51:03 +0200

    add task 3

---
+++ js/03-lazysizes.js
@@ -0,0 +1,7 @@
+// import { galleryItems } from './gallery-items.js';
+// const listEl = document.querySelector('.gallery');
+
+
+// const img = galleryItems.map(({original}) => `<img src='${original}' loading="lazy"/> width="960px" height="800px"`).join('');
+
+// listEl.insertAdjacentHTML('beforeend', img);
