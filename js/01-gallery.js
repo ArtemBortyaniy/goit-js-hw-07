39cbb7934b03427f6c4d5ff078ddf4453b5dc2c9
Artem <artembariga5@gmail.com>
2023-02-18 21:07:44 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,45 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
 
 const listEl = document.querySelector('.gallery');
 
-function renderImage (galleryItems) {
+let instance = basicLightbox.create(`
+    <img src="" width="800" height="600">`, { 
+        closable: true, 
+    });
+
+function renderElImages (galleryItems) {
     const imagesGallery = []
 
-    for(const elem of galleryItems) {
-        imagesGallery.push(`<a class="gallery__link" href="large-image.jpg">
-        <img src='${elem.preview}' alt='${elem.description}' data-source='${elem.original}' style='width: 340px'/>
+    for(const {preview, description, original} of galleryItems) {
+        imagesGallery.push(`<a class="gallery__link" href="${original}">
+        <img src='${preview}' alt='${description}' data-source='${original}' style='width: 340px'/>
         </a>`);
     }
 
     return imagesGallery.join('');
 }
 
-listEl.insertAdjacentHTML('beforeend', renderImage(galleryItems));
+listEl.insertAdjacentHTML('beforeend', renderElImages(galleryItems));
 
+function openElGallery (event) {
+    event.preventDefault();
 
-function openElemGallery (event) {
     if(event.target.nodeName !== 'IMG') {
         return;
     }
     
-    const targetImagOrigin = event.target.dataset.source;
+    const targetImgOrigin = event.target.dataset.source;
 
-    const instance = basicLightbox.create(`
-    <img src="${targetImagOrigin}" width="800" height="600">
-    `);
-
-    instance.show()
-
-    document.addEventListener("keydown", event => {
-        if(event.code === 'Escape') {
-            instance.close();
-        }
+    instance = basicLightbox.create(`
+    <img src="${targetImgOrigin}" width="800" height="600">`, { 
+        closable: true, 
     });
+
+    instance.show();
 }
 
-listEl.addEventListener('click', openElemGallery);
+listEl.addEventListener('click', openElGallery);
 
+function closeElGallery (event) {
+    if(event.code === 'Escape') {
+        instance.close();
+    }
+}
+
+document.addEventListener('keydown', closeElGallery);
+
+
+// console.log(galleryItems.map(({preview, description, original}) => {
+//     `<a class="gallery__link" href="${original}">
+//         <img src='${preview}' alt='${description}' data-source='${original}' style='width: 340px'/>
+//     </a>`;
+// }));
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const listEl = document.querySelector('.gallery');
-
-console.log(galleryItems);
-
-function renderGallery (gallery) {
-    let str = `<a class="gallery__item" href="${gallery.original}" >
-        <img class="gallery__image" src="${gallery.preview}" alt="${gallery.description}" title="${gallery.description}"/>
-        </a>`;
-
-    return str;
-}
-
 const dataGallery = galleryItems.map(renderGallery).join('');
+
+
+function renderGallery ({original, preview, description}) {
+    return`
+    <li>
+        <a class="gallery__item" href="${original}" >
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a>
+    </li>
+    `;
+}
 
 listEl.insertAdjacentHTML('beforeend', dataGallery);
 
-var lightbox = new SimpleLightbox('.gallery a', { title:250});
+new SimpleLightbox('.gallery a', { captionDelay:250});
 
 
 
