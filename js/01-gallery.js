6141125a82a2275e0386639f325085b40c04244b
Artem <artembariga5@gmail.com>
2023-02-20 00:00:45 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,56 +2,92 @@ import { galleryItems } from './gallery-items.js';
 
 const listEl = document.querySelector('.gallery');
 
-let instance = basicLightbox.create(`
-    <img src="" width="800" height="600">`, { 
-        closable: true, 
-    });
-
-function renderElImages (galleryItems) {
-    const imagesGallery = []
-
-    for(const {preview, description, original} of galleryItems) {
-        imagesGallery.push(`<a class="gallery__link" href="${original}">
+const galleryMarkap = galleryItems.map(({preview, description, original}) => {
+    return `<a class="gallery__link" href="${original}">
         <img src='${preview}' alt='${description}' data-source='${original}' style='width: 340px'/>
-        </a>`);
-    }
+            </a>`;
+}).join('');
 
-    return imagesGallery.join('');
-}
+listEl.insertAdjacentHTML('beforeend', galleryMarkap);
 
-listEl.insertAdjacentHTML('beforeend', renderElImages(galleryItems));
+listEl.addEventListener('click', clickOnContainer);
 
-function openElGallery (event) {
+function clickOnContainer (event) {
     event.preventDefault();
 
     if(event.target.nodeName !== 'IMG') {
         return;
     }
-    
-    const targetImgOrigin = event.target.dataset.source;
 
-    instance = basicLightbox.create(`
-    <img src="${targetImgOrigin}" width="800" height="600">`, { 
-        closable: true, 
-    });
-
-    instance.show();
-}
-
-listEl.addEventListener('click', openElGallery);
-
-function closeElGallery (event) {
-    if(event.code === 'Escape') {
-        instance.close();
+    const closeModal = event => {
+        if(event.code === 'Escape') {
+            lightboxInstance.close();
+        }
     }
+
+    const largeImgUrl = event.target.dataset.source;
+
+    const lightboxInstance = basicLightbox.create(`
+    <img src="${largeImgUrl}">`, {
+        onShow: () => {
+            document.addEventListener('keydown', closeModal);
+        },
+        onClose: () => {
+            document.removeEventListener('keydown', closeModal);
+        }
+    })
+
+    lightboxInstance.show();
 }
 
-document.addEventListener('keydown', closeElGallery);
 
+// const listEl = document.querySelector('.gallery');
 
-// console.log(galleryItems.map(({preview, description, original}) => {
-//     `<a class="gallery__link" href="${original}">
+// let instance = basicLightbox.create(`
+//     <img src="" width="800" height="600">`, { 
+//         closable: true, 
+//     });
+
+// function renderElImages (galleryItems) {
+//     const imagesGallery = []
+
+//     for(const {preview, description, original} of galleryItems) {
+//         imagesGallery.push(`<a class="gallery__link" href="${original}">
 //         <img src='${preview}' alt='${description}' data-source='${original}' style='width: 340px'/>
-//     </a>`;
-// }));
+//         </a>`);
+//     }
+
+//     return imagesGallery.join('');
+// }
+
+// listEl.insertAdjacentHTML('beforeend', renderElImages(galleryItems));
+
+// function openElGallery (event) {
+//     event.preventDefault();
+
+//     if(event.target.nodeName !== 'IMG') {
+//         return;
+//     }
+    
+//     const targetImgOrigin = event.target.dataset.source;
+
+//     instance = basicLightbox.create(`
+//     <img src="${targetImgOrigin}" width="800" height="600">`, { 
+//         closable: true, 
+//     });
+
+//     instance.show();
+// }
+
+// listEl.addEventListener('click', openElGallery);
+
+// function closeElGallery (event) {
+//     if(event.code === 'Escape') {
+//         instance.close();
+//     }
+// }
+
+// document.addEventListener('keydown', closeElGallery);
+
+
 
